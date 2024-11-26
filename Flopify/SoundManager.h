e6e8270bbfe7ed1f86c37d4d0df60267d87904d9
naoki18 <jadras.dunkan@gmail.com>
2024-11-26 17:18:22 +0100

    Fix Play

--- Flopify/SoundManager.h
+++ Flopify/SoundManager.h
@@ -2,9 +2,13 @@
 class SoundManager
 {
 public :
-	static  SoundManager& Get();
+	static SoundManager& Get();
 	bool Play(const char* path);
 private :
 	SoundManager() = default;
+	~SoundManager();
+
+	unsigned int uiBuffer;
+	unsigned int uiSource;
 };
 
