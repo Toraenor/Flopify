70ca6ffc6448ec55dd2059ebc99a84dceb67b273
naoki18 <jadras.dunkan@gmail.com>
2024-12-06 11:10:36 +0100

    ogg

--- Flopify/SoundManager.h
+++ Flopify/SoundManager.h
@@ -1,7 +1,7 @@
 #pragma once
 class SoundManager
 {
-public :
+public:
 	static SoundManager& Get();
 	bool Play(const char* path);
 	void Pause();
@@ -12,11 +12,16 @@ public :
 	void ChangeMusicTime(float newTime);
 	float GetMusicDuration();
 	float GetCurrentMusicTime();
-private :
+
+private:
 	SoundManager();
 	~SoundManager();
 
-	void PlayOGG();
+	void PlayOGG(const char* path);
+	void InitVorbisFile();
+	void ShutdownVorbisFile();
+	unsigned long DecodeOggVorbis(class OggVorbis_File* psOggVorbisFile, char* pDecodeBuffer, unsigned long ulBufferSize, unsigned long ulChannels);
+	void Swap(short& s1, short& s2);
 
 	unsigned int      uiBuffer;
 	unsigned int      uiSource;
